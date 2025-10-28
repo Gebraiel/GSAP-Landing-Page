4b745afa219f4e4132565202c9883b3b37abac31
RasansRise <67439330+Gebraiel@users.noreply.github.com>
2025-10-28 18:10:57 +0300

    upload the work

---
+++ assets/js/index.js
@@ -0,0 +1,46 @@
+const tl = gsap.timeline({
+    defaults:{
+        duration:0.7,
+        ease: "power3.out"
+
+    }
+});
+tl.from(".white-overlay",{
+    opacity:0,
+    scaleY:0,
+    stagger:0.3,
+    delay:0.3,
+    transformOrigin:"top center"
+})
+tl.fromTo(".category img",{opacity:0,scale:1.1},{
+    opacity:1,
+    scale:1,
+    stagger:0.3,
+    duration:1,
+    ease: "power4.out"
+})
+tl.from(".content .title",{
+    opacity:0,
+    yPercent:100,
+})
+tl.from(".content .subtitle",{
+    opacity:0,
+    yPercent:100,
+   
+})
+tl.from("header",{
+    height:0,
+    opacity:0,
+})
+tl.from("header .black-overlay",{
+    yPercent:-100,
+})
+tl.from(".logo",{
+    yPercent:-100,
+    opacity:0,
+})
+tl.fromTo(".nav-item",{opacity:0},{
+    opacity:1,
+    duration:0.2,
+    stagger:0.2
+})
